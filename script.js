b38f0f9ec33ade2d77091dde6db9d55297dd4939
Paolo Jesse Nwia Azane <100587066+Jessenwia23@users.noreply.github.com>
2025-06-30 00:05:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Welcome to The Princess and Mr Machete app");
+
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+document.getElementById("bibleJournalForm").addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const verse = document.getElementById("bibleVerse").value.trim();
+  const note = document.getElementById("bibleNote").value.trim();
+
+  if (verse && note) {
+    await addDoc(collection(window.db, "bibleJournal"), {
+      verse,
+      note,
+      timestamp: Date.now()
+    });
+    alert("Entry saved to Firebase!");
+    bibleJournalForm.reset();
+  }
+});
